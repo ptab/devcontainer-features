9be7db7daa15bb00b1d8392e5ae120b4a08d2ab3
Pedro Taborda <pedro@taborda.me>
2025-09-19 10:16:21 +0200

    removed features in progress

--- src/metals/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eu
-
-echo "Installing Coursier..."
-curl -fL "https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz" | gzip -d >/usr/local/bin/coursier
-chmod +x /usr/local/bin/coursier
-/usr/local/bin/coursier setup --yes
-
-echo "Coursier and apps installed successfully."
