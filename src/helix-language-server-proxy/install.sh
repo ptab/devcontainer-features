f44d7759a395de19807b15b853d88657e0768299
Pedro Taborda <pedro@taborda.me>
2025-09-19 10:16:20 +0200

    initial commit

---
+++ src/helix-language-server-proxy/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -eu
+
+cp ./helix-language-server-proxy /usr/local/bin/helix-language-server-proxy
+cp ./proxy.py /usr/local/bin/helix-lsp-proxy.py
+chmod +x /usr/local/bin/helix-language-server-proxy
